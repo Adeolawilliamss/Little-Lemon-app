e7f6aafb1586646032d1f38d323b4781bea1dc30
Adeolawiilliamss <babagenerah@gmail.com>
2024-03-07 23:34:59 +0100

    Initial first stage completed

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+    function Header(){
+    return(
+        <header>
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <img src='LittleLemonLogo.png' alt='Little Lemon Logo'/>
+        </header>
+    )
+}
+
+export default Header;
